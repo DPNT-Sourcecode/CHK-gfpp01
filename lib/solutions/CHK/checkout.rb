15581de9ee992aa008d5b99ad54d98bec528dca4
sbx_user1051 <sbx_user1051@169.254.200.249>
2023-10-17 17:54:36 +0000

    Tue Oct 17 17:54:36 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,23 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
+  def initialize
+    @price_table = {
+      "A" => {:price => 50, :offer => {:quantity => 3, :offer_price => 130}},
+      "B" => {:price => 30, :offer => {:quantity => 2, :offer_price => 45}},
+      "C" => {:price => 20},
+      "D" => {:price => 15}
+    }
+  end
 
   def checkout(skus)
-    raise 'Not implemented'
+    # Check for illegal input
+
+    item_count = skus.split("").each_with_object(Hahs.new(0)) do |sku, hash|
+      hash[sku] += 1
+    end
+
+
   end
 
 end
+
