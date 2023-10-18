a912a781aa34ad576c2dd30609b490bcd8b2acfc
sbx_user1051 <sbx_user1051@169.254.179.129>
2023-10-18 10:44:31 +0000

    Wed Oct 18 10:44:31 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -16,7 +16,7 @@ class Checkout
       "U" => [{:quantity => 4, :free_sku => "U"}]
     }
     @group_offer_table = {
-
+      "ZYTSX" => {:quantity => 3, :offer_price => 45}
     }
     @price_offer_table = {
       "A" => [{:quantity => 3, :offer_price => 130}, {:quantity => 5, :offer_price => 200}],
@@ -33,8 +33,8 @@ class Checkout
   def checkout(skus)
     if valid_input?(skus)
       @total_price = 0
-      set_item_count(skus)
-      apply_free_sku_special_offers(skus)
+      update_initial_item_count(skus)
+      apply_free_sku_special_offers
       calculate_total_price
     end
     @total_price
@@ -48,7 +48,7 @@ class Checkout
     skus.delete(VALID_PRODUCTS).empty?
   end
 
-  def set_item_count(skus)
+  def update_initial_item_count(skus)
     @item_count = skus.split("").each_with_object(Hash.new(0)) do |sku, hash|
       hash[sku] += 1
     end
@@ -57,13 +57,13 @@ class Checkout
   def calculate_total_price
     @item_count.each do |sku, count|
       @count = count
-      apply_special_offer_prices(sku, price_table[sku][:offer]) if price_table[sku].key?(:offer)
+      apply_special_offer_prices(price_table[sku][:offer]) if price_table[sku].key?(:offer)
       @total_price += @count * price_table[sku][:price]
     end
     @total_price
   end
 
-  def apply_special_offer_prices(sku, offer)
+  def apply_special_offer_prices(offer)
     if offer.is_a?(Array)
       offer.select { |o| o.key?(:offer_price) }.sort_by { |o| o[:quantity] }.reverse_each do |o|
         apply_special_offer_price(o)
@@ -80,28 +80,26 @@ class Checkout
     end
   end
 
-  def apply_free_sku_special_offers(skus)
+  def apply_free_sku_special_offers
     @item_count_updates = Hash.new(0)
     @item_count.each do |sku, count|
       @count = count
-      if price_table[sku].key?(:offer)
-        apply_free_sku_special_offer(sku, price_table[sku][:offer])
-      end
+      apply_free_sku_special_offer(price_table[sku][:offer]) if price_table[sku].key?(:offer)
     end
     update_item_count
   end
 
-  def apply_free_sku_special_offer(sku, offer)
+  def apply_free_sku_special_offer(offer)
     if offer.is_a?(Hash) && offer.key?(:free_sku)
-      update_item_count_updates(sku, offer)
+      update_item_count_updates(offer)
     elsif offer.is_a?(Array)
       offer.select { |o| o.key?(:free_sku) }.sort_by { |o| o[:quantity] }.reverse_each do |o|
-        update_item_count_updates(sku, o)
+        update_item_count_updates(o)
       end
     end
   end
 
-  def update_item_count_updates(sku, offer)
+  def update_item_count_updates(offer)
     while @count >= offer[:quantity]
       @count -= offer[:quantity]
       @item_count_updates[offer[:free_sku]] -= 1
@@ -110,7 +108,7 @@ class Checkout
 
   def update_item_count
     @item_count_updates.each do |sku, update_count|
-      next unless @item_count[sku] > 0
+      next unless @item_count[sku].positive?
 
       if @item_count[sku] + update_count >= 0
         @item_count[sku] += update_count
@@ -120,5 +118,6 @@ class Checkout
     end
   end
 end
+
 
 
